80fba721cefed7bbce89c38e78ec609ebf5b2ae7
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-01 02:33:37 +0800

    techpack: mi8953: ts: fts: Fix section mismatch on `fts_ts_{i2c,spi}_init()`
    Change-Id: Ifdaef8c34dba200c01089f4e4879572a4adbca2f

--- touchscreen/focaltech_touch/focaltech_core.c
+++ touchscreen/focaltech_touch/focaltech_core.c
@@ -3467,7 +3467,7 @@ static struct i2c_driver fts_ts_i2c_driver = {
 	.id_table = fts_ts_i2c_id,
 };
 
-static int __init fts_ts_i2c_init(void)
+static int fts_ts_i2c_init(void)
 {
 	int ret = 0;
 
@@ -3565,7 +3565,7 @@ static struct spi_driver fts_ts_spi_driver = {
 	.id_table = fts_ts_spi_id,
 };
 
-static int __init fts_ts_spi_init(void)
+static int fts_ts_spi_init(void)
 {
 	int ret = 0;
 
