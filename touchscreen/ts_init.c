348453feddb71567a277aa675a4ec4940fc0d940
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-03-31 13:50:31 +0000

    techpack: mi8953: ts: Call each drivers' init functions from ts_init.c

--- touchscreen/ts_init.c
+++ touchscreen/ts_init.c
@@ -10,3 +10,17 @@
 */
 bool xiaomi_msm8953_touchscreen_is_probed = false;
 EXPORT_SYMBOL(xiaomi_msm8953_touchscreen_is_probed);
+
+#if IS_ENABLED(CONFIG_TOUCHSCREEN_FTS_MI8953)
+extern int xiaomi_msm8953_touchscreen_fts_ts_init(void);
+#endif
+
+static int __init xiaomi_msm8953_touchscreen_init(void)
+{
+#if IS_ENABLED(CONFIG_TOUCHSCREEN_FTS_MI8953)
+    xiaomi_msm8953_touchscreen_fts_ts_init();
+#endif
+
+    return 0;
+}
+module_init(xiaomi_msm8953_touchscreen_init);
